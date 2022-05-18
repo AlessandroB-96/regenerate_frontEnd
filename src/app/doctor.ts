37104d0a10d6ebcc1910212eb5e2a305ae8af352
Veronica <veronica.papini1997@gmail.com>
2022-05-18 12:14:49 +0200

    doctors card updated

--- src/app/doctor.ts
+++ src/app/doctor.ts
@@ -1,7 +1,11 @@
+import { Department } from "./department";
+
 export interface Doctor {
     idDoc: number;
     name: string;
     surname: string;
     phone: number;
     amOrPm: boolean;
+    idDepartment: Department;
+    imageUrl: string;
 }
