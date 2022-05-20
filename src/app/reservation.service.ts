7f459edcbad48a230d497474b28821d2178647b5
Alessandro Brun <univale9966aa@gmail.com>
2022-05-20 17:42:00 +0200

    Small changes

--- src/app/reservation.service.ts
+++ src/app/reservation.service.ts
@@ -1,9 +1,13 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ReservationService {
 
-  constructor() { }
+  private apiServerUrl = environment.apiBaseUrl;
+
+  constructor(private http: HttpClient) { }
 }
